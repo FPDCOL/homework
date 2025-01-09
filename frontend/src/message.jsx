776cb5645797469a7f22a5190334de65192beb62
FPDCOL <00542651@dudleylearners.ac.uk>
2025-01-09 21:59:04 +0000

    done

--- frontend/src/message.jsx
+++ frontend/src/message.jsx
@@ -3,25 +3,16 @@ import {Link} from 'react-router-dom';
 
 import './index.css'
 
-GetMessage();{
-    fetch('http://localhost:3000', {
-            method: "GET",
-        })
-        .then(response => console.log(response))
-        .then((response) => {
-            console.log(response.data)
-            this.setState({products: response.data});
-}}
-
+//get server message 
 
 const MESSAGE = () => {
     return (
       <div style={{backgroundColor:'#36143d', display:'flex', flexDirection:'column',alignItems: 'center', height:'100vh'}}>
-        <h2 className='fancy'> place holder</h2>
-        <p></p>
-        <Link to='/message'><button className='btn btn-primary mt-3 px-4 py-2 fancy'>close Card</button></Link>
+        <h2 className='mt-3 fancy' style={{marginTop:'50px'}}> Happy new year! </h2>
+        <p className='fancy mt-2'>May it bring opurtunities and good fortune</p>
+        <Link to='/letter'><button className='btn btn-primary mt-3 px-4 py-2 fancy'>Close Card</button></Link>
       </div>
     )
-  }
+}
   
-  export default MESSAGE
+  export default MESSAGE;
